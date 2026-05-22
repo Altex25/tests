49fca22df65ab75012ea363e976b3a611c65ae46
alexandre <alexandre.py25@free.fr>
2026-05-22 15:58:47 +0200

    Add content to exercice 6

--- exercice_6/src/main/java/org/example/RechercheVille.java
+++ exercice_6/src/main/java/org/example/RechercheVille.java
@@ -3,5 +3,23 @@ package org.example;
 import java.util.List;
 
 public class RechercheVille {
-    public List<String> rechercher(String mot) { return null; }
+    private final List<String> villes = List.of(
+            "Paris", "Budapest", "Skopje", "Rotterdam", "Valence",
+            "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York",
+            "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul"
+    );
+
+    public List<String> rechercher(String mot) {
+        if (mot == null || (!"*".equals(mot) && mot.length() < 2)) {
+            throw new NotFoundException("Le texte de recherche doit contenir au moins 2 caractères.");
+        }
+        if ("*".equals(mot)) {
+            return villes;
+        }
+        String motLower = mot.toLowerCase();
+
+        return villes.stream()
+                .filter(ville -> ville.toLowerCase().contains(motLower))
+                .toList();
+    }
 }
