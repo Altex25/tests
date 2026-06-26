8f018b895f597f29ed8234f7b269ecd7647d2dd5
alexandre <alexandre.py25@free.fr>
2026-06-26 10:32:18 +0200

    feat: add GitHub action ex 13

--- exercice_13/src/test/java/org/example/exercice_13/bdd/RunCucumberTest.java
+++ exercice_13/src/test/java/org/example/exercice_13/bdd/RunCucumberTest.java
@@ -14,6 +14,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example.exercice_13.bdd")
 @ConfigurationParameter(
         key = PLUGIN_PROPERTY_NAME,
-        value = "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json")
+        value = "pretty, html:target/cucumber-reports/cucumber.html, json:target/cucumber-reports/cucumber.json")
 public class RunCucumberTest {
 }
